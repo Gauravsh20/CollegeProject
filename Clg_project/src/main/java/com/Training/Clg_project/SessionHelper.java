9f7f9a820a746aefb7a96aed69069862146fec2f
gauravsh20 <s.gauravmbd@gmail.com>
2022-10-25 14:28:51 +0530

    jdsk

---
+++ Clg_project/src/main/java/com/Training/Clg_project/SessionHelper.java
@@ -0,0 +1,14 @@
+package com.Training.Clg_project;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class SessionHelper {
+	public static SessionFactory getConnection() {
+		Configuration cfg=new AnnotationConfiguration().configure();
+		SessionFactory sf=cfg.buildSessionFactory();
+		return sf;
+		
+	}
+}
